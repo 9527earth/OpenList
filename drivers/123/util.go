bca0ad2572a9d5a0b8d51a39fc767ae04ea72b0f
9527earth <176719848+9527earth@users.noreply.github.com>
2025-07-15 06:06:35 +0800

    Update util.go
    Signed-off-by: 9527earth <176719848+9527earth@users.noreply.github.com>

--- drivers/123/util.go
+++ drivers/123/util.go
@@ -202,9 +202,10 @@ do:
 		"origin":        "https://www.123pan.com",
 		"referer":       "https://www.123pan.com/",
 		"authorization": "Bearer " + d.AccessToken,
-		"user-agent":    "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) openlist-client",
-		"platform":      "web",
-		"app-version":   "3",
+		"user-agent":    "123pan/v2.5.10(Android_14.0.0;Xiaomi)",
+		"platform":      "android",
+		"app-version":   "83",
+		"x-app-version": "2.5.10",
 		//"user-agent":    base.UserAgent,
 	})
 	if callback != nil {
