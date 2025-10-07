e0db57e5ed44ae2019b95e574b361377d9aef6b8
9527earth <176719848+9527earth@users.noreply.github.com>
2025-10-07 13:17:57 +0800

    Update util.go
    Signed-off-by: 9527earth <176719848+9527earth@users.noreply.github.com>

--- drivers/123/util.go
+++ drivers/123/util.go
@@ -163,9 +163,9 @@ func (d *Pan123) login() error {
 		SetHeaders(map[string]string{
 			"origin":      "https://www.123pan.com",
 			"referer":     "https://www.123pan.com/",
-			"user-agent":  "Dart/2.19(dart:io)-openlist",
-			"platform":    "web",
-			"app-version": "3",
+		    "user-agent":    "123pan/v3.0.0(Android_14.0.0;Xiaomi)",
+	    	"platform":      "android",
+	    	"app-version":   "300",
 			//"user-agent":  base.UserAgent,
 		}).
 		SetBody(body).Post(SignIn)
